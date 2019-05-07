e4d2f3ee2697c4200b25c817d3fad0c4b86d28b8
LayerX <qdao.cntt@gmail.com>
2019-05-07 14:40:03 +0700

    Đạo - template cơ bản
    template

---
+++ src/main/java/vn/cdw/cdwforums/CdwForumsApplication.java
@@ -0,0 +1,13 @@
+package vn.cdw.cdwforums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdwForumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdwForumsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/vn/cdw/cdwforums/controller/HomController.java
@@ -0,0 +1,13 @@
+package vn.cdw.cdwforums.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomController {
+
+	@GetMapping("/")
+	public String getHome() {
+		return "forum/home";
+	}
+}
