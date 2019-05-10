f791d43db427272f77e1c7e4e63bb3f19c252ff2
tqdaoinfo108 <50355596+tqdaoinfo108@users.noreply.github.com>
2019-05-10 16:01:53 +0700

    Đạo - template cơ bản ( 2)

--- src/main/java/vn/cdw/cdwforums/controller/HomController.java
+++ src/main/java/vn/cdw/cdwforums/controller/HomController.java
@@ -10,4 +10,33 @@ public class HomController {
 	public String getHome() {
 		return "forum/home";
 	}
+	@GetMapping("/profile")
+	public String getProfile() {
+		return "forum/user-profile";
+	}
+	
+	@GetMapping("/loggin")
+	public String getLogin() {
+		return "forum/user-login";
+	}
+	
+	@GetMapping("/resetpassword")
+	public String getResetPass() {
+		return "forum/user-resetpass";
+	}
+	
+	@GetMapping("/register")
+	public String getRegister() {
+		return "forum/user-register";
+	}
+	
+	@GetMapping("/educ-timeline")
+	public String getEductime() {
+		return "forum/educ-timeline";
+	}
+	
+	@GetMapping("/educ-timeline-detail")
+	public String getEductimeDetail() {
+		return "forum/educ-timeline-detail";
+	}
 }
