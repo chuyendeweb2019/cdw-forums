0882ded7f05d03f39f5556c6d56a05a7f4f78369
tqdaoinfo108 <50355596+tqdaoinfo108@users.noreply.github.com>
2019-05-28 13:06:09 +0700

    híc

--- src/main/java/vn/cdw/cdwforums/service/UserService.java
+++ src/main/java/vn/cdw/cdwforums/service/UserService.java
@@ -43,6 +43,21 @@ public class UserService implements UserDetailsService {
         }
         return user;
     }
+    public boolean loadByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)) {
+        	 return false;
+        }
+        return true;
+    }
+
+    public boolean loadUserByEmail(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (Objects.isNull(user)) {
+            return false;
+        }
+        return true;
+    }
 
     public void signupUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -58,6 +73,8 @@ public class UserService implements UserDetailsService {
         if (userDetails instanceof User) {
             user = (User) userDetails;
         }
+        
+        System.out.println("-------------------------"+user.getRoles());
         return user;
     }
 
@@ -70,7 +87,7 @@ public class UserService implements UserDetailsService {
         Object object = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (Objects.nonNull(object)) {
             try {
-                UserDetails userDetails = (UserDetails) object;
+                UserDetails userDetails =  (UserDetails) object;
                 User user;
                 if (userDetails instanceof User) {
                     user = (User) userDetails;
@@ -85,9 +102,8 @@ public class UserService implements UserDetailsService {
         return null;
     }
 
-    @SuppressWarnings("unchecked")
 	public boolean hasRole(String role) {
-        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>)
+        Collection<? extends GrantedAuthority> authorities = 
                 SecurityContextHolder.getContext().getAuthentication().getAuthorities();
 
         boolean hasRole = false;
@@ -97,6 +113,8 @@ public class UserService implements UserDetailsService {
                 break;
             }
         }
+        System.out.println("-------------------------"+role);
+
         return hasRole;
     }
 }
